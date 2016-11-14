1a3f40fa02f75bb760c85677dc196726156b9bd0
张智彬 <zhangzhibin@yaochufa.com>
2016-11-14 18:33:42 +0800

    初步完成RoundedBackgroundSpan

--- app/src/main/java/com/benio/demoproject/span/RoundedBackgroundSpan.java
+++ app/src/main/java/com/benio/demoproject/span/RoundedBackgroundSpan.java
@@ -1,56 +1,99 @@
 package com.benio.demoproject.span;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
-import android.text.style.LineBackgroundSpan;
+import android.graphics.RectF;
+import android.support.annotation.NonNull;
+import android.text.style.ReplacementSpan;
+import android.util.Log;
 
-//public class RoundedBackgroundSpan extends ReplacementSpan {
-//
-//
-//    @Override
-//    public void draw(Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom, Paint paint) {
-//        RectF rect = new RectF(x, top, x + measureText(paint, text, start, end), bottom);
-//        paint.setColor(Color.BLUE);
-//        canvas.drawRoundRect(rect, 100f, 30f, paint);
-//        paint.setColor(Color.BLACK);
-//        canvas.drawText(text, start, end, x, y, paint);
-//    }
-//
-//    @Override
-//    public int getSize(Paint paint, CharSequence text, int start, int end, Paint.FontMetricsInt fm) {
-//        return Math.round(measureText(paint, text, start, end));
-//    }
-//
-//    private float measureText(Paint paint, CharSequence text, int start, int end) {
-//        return paint.measureText(text, start, end);
-//    }
-//
-//}
-public class RoundedBackgroundSpan implements LineBackgroundSpan {
+public class RoundedBackgroundSpan extends ReplacementSpan {
+    private static final String TAG = "xxxx";
+    private int mPaddingLeft;
+    private int mPaddingTop;
+    private int mPaddingRight;
+    private int mPaddingBottom;
+
+    private int mMarginLeft;
+    private int mMarginTop;
+    private int mMarginRight;
+    private int mMarginBottom;
+
+    private RectF mRectF;
     private int mBackgroundColor;
-    private int mPadding;
-    private Rect mBgRect;
+    private int mRadius;
+    private Paint mPaint;
+    private boolean[] mIsDigitArray;
+    private int mSpanWidth = -1;
 
-    public RoundedBackgroundSpan(int backgroundColor, int padding) {
-        super();
-        mBackgroundColor = backgroundColor;
-        mPadding = padding;
-        // Precreate rect for performance
-        mBgRect = new Rect();
+    public RoundedBackgroundSpan() {
+        mRadius = 10;
+        mMarginLeft = mMarginRight = 8;
+        mPaddingLeft = mPaddingRight = 10;
+        mBackgroundColor = Color.YELLOW;
+        mRectF = new RectF();
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(mBackgroundColor);
     }
 
     @Override
-    public void drawBackground(Canvas c, Paint p, int left, int right, int top, int baseline, int bottom, CharSequence text, int start, int end, int lnum) {
-        final int textWidth = Math.round(p.measureText(text, start, end));
-        final int paintColor = p.getColor();
-        // Draw the background
-        mBgRect.set(left - mPadding,
-                top - (lnum == 0 ? mPadding / 2 : - (mPadding / 2)),
-                left + textWidth + mPadding,
-                bottom + mPadding / 2);
-        p.setColor(mBackgroundColor);
-        c.drawRect(mBgRect, p);
-        p.setColor(paintColor);
+    public int getSize(@NonNull Paint paint, CharSequence text, int start, int end, Paint.FontMetricsInt fm) {
+        if (mSpanWidth < 0) {
+            String subString = text.subSequence(start, end).toString();
+            boolean[] digit = new boolean[subString.length()];
+            int horizontalPadding = mPaddingLeft + mPaddingRight;
+            int horizontalMargin = mMarginLeft + mMarginRight;
+            int digitNum = 0;
+            for (int i = start; i < end; i++) {
+                char charAt = subString.charAt(i);
+                boolean isDigit = Character.isDigit(charAt);
+                digit[i] = isDigit;
+                if (isDigit) {
+                    digitNum++;
+                }
+            }
+            mIsDigitArray = digit;
+            mSpanWidth = (int) paint.measureText(text, start, end) + digitNum * horizontalPadding + (digitNum - 1) * horizontalMargin;
+        }
+        Log.d(TAG, "getSize: " + mSpanWidth + "," + start + "," + end);
+        return mSpanWidth;
+    }
+
+    @Override
+    public void draw(@NonNull Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom, @NonNull Paint paint) {
+        Log.d(TAG, "draw() called with: canvas = [" + canvas + "], text = [" + text + "], start = [" + start + "], end = [" + end + "], x = [" + x + "], top = [" + top + "], y = [" + y + "], bottom = [" + bottom + "], paint = [" + paint + "]");
+//        float charx = x;
+//        char[] chars = text.subSequence(start, end).toString().toCharArray();
+//        for (int i = 0; i < chars.length; i++) {
+//            final char charAt = chars[i];
+//            float charWidth = paint.measureText(chars, i, 1);
+//            mRectF.left = charx + mMarginLeft;
+//            mRectF.top = top;
+//            mRectF.right = mRectF.left + charWidth + mPaddingLeft + mPaddingRight;
+//            mRectF.bottom = bottom;
+//            if (Character.isDigit(charAt)) {
+//                canvas.drawRoundRect(mRectF, mRadius, mRadius, mPaint);
+//            } else {
+//                // 去除上一个字符多加的padding
+//                mRectF.left = charx + mMarginLeft - mPaddingLeft - mPaddingRight;
+//                mRectF.right = mRectF.left + charWidth;
+//            }
+//
+//            canvas.drawText(String.valueOf(charAt), 0, 1, mRectF.left + mPaddingLeft, y, paint);
+//            charx = mRectF.right + mMarginRight;
+//            Log.d(TAG, "draw: " + charWidth + "," + mRectF);
+//        }
+
+        String subString = text.subSequence(start, end).toString();
+        float charx = x;
+        for (int i = start; i < end; i++) {
+
+        }
+
+    }
+
+    private static String extractText(CharSequence text, int start, int end) {
+        return text.subSequence(start, end).toString();
     }
 }
